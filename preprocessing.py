e88e194edfb0ce65fbab8ec58a84888f8304f9ec
Nishank Sharma <nishankatwork@gmail.com>
2018-10-31 21:00:22 -0600

    Commit 10/31

---
+++ preprocessing.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv('./fer2013.csv')
+
+width, height = 48, 48
+
+datapoints = data['pixels'].tolist()
+
+#getting features for training
+X = []
+for xseq in datapoints:
+    xx = [int(xp) for xp in xseq.split(' ')]
+    xx = np.asarray(xx).reshape(width, height)
+    X.append(xx.astype('float32'))
+
+X = np.asarray(X)
+X = np.expand_dims(X, -1)
+
+#getting labels for training
+y = pd.get_dummies(data['emotion']).as_matrix()
+
+#storing them using numpy
+np.save('fdataX', X)
+np.save('flabels', y)
+
+print("Preprocessing Done")
+print("Number of Features: "+str(len(X[0])))
+print("Number of Labels: "+ str(len(y[0])))
+print("Number of examples in dataset:"+str(len(X)))
+print("X,y stored in fdataX.npy and flabels.npy respectively")
