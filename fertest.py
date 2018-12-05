f3cba14cc399429982eeafde178b37a09b92a65f
yasa0041 <yasa0041@colorado.edu>
2018-12-05 03:31:58 +0000

    update model to support TPU on Google Cloud

--- fertest.py
+++ fertest.py
@@ -1,18 +1,14 @@
 # load json and create model
 from __future__ import division
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.models import model_from_json
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import model_from_json, load_model
 import numpy
 import os
 import numpy as np
 
-json_file = open('fer.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("fer.h5")
+loaded_model = load_model('tpu_fer.h5')
+
 print("Loaded model from disk")
 
 truey=[]
